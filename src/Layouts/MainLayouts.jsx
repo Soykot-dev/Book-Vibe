2495f5e369cbac8ff2018ba1d1458754127540e3
Sabbir Ahmed Soykot <soykotislam131415@gmail.com>
2025-01-14 13:27:24 +0600

    add NavBar and Footer components; integrate them into MainLayouts and update styles

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../Components/Shared/NavBar/NavBar";
+import Footer from "../Components/Shared/Footer/Footer";
+
 const MainLayouts = () => {
     return (
         <div>
-            
+            <NavBar />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
