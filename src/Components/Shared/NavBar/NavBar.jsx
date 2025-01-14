9a4944066478f6df40266dc3f079cb343979dcfd
Sabbir Ahmed Soykot <soykotislam131415@gmail.com>
2025-01-14 15:46:27 +0600

    refactor NavBar component for improved responsiveness and styling consistency

--- src/Components/Shared/NavBar/NavBar.jsx
+++ src/Components/Shared/NavBar/NavBar.jsx
@@ -13,19 +13,19 @@ const NavBar = () => {
 
     const links =
         <>
-            <li><NavLink to='/' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-md px-4 py-2 text-green-500 hover:text-green-600 duration-100' : 'text-natural hover:underline duration-100'}>Home</NavLink></li>
-            <li><NavLink to='/listed-book' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-md px-4 py-2 text-green-500 hover:text-green-600 duration-100' : 'text-natural hover:underline duration-100'}>Listed Book</NavLink></li>
-            <li><NavLink to='/pages-to-read' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-md px-4 py-2 text-green-500 hover:text-green-600 duration-100' : 'text-natural hover:underline duration-100'}>Pages To Read</NavLink></li>
+            <li><NavLink to='/' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-sm md:rounded-md px-4 md:py-2 text-green-500 hover:text-green-600 duration-100 text-xs md:text-base' : 'text-natural hover:underline duration-100 text-xs md:text-base'}>Home</NavLink></li>
+            <li><NavLink to='/listed-book' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-sm md:rounded-md px-4 md:py-2 text-green-500 hover:text-green-600 duration-100 text-xs md:text-base' : 'text-natural hover:underline duration-100 text-xs md:text-base'}>Listed Book</NavLink></li>
+            <li><NavLink to='/pages-to-read' className={({ isActive }) => isActive ? 'border border-green-500 font-semibold rounded-sm md:rounded-md px-4 md:py-2 text-green-500 hover:text-green-600 duration-100 text-xs md:text-base' : 'text-natural hover:underline duration-100 text-xs md:text-base'}>Pages To Read</NavLink></li>
         </>
 
     return (
         <div className="navbar bg-base-100 shadow-md font-lato">
             <div className="navbar-start">
-                <div className="dropdown">
+                <div className="dropdown -ml-5 md:ml-0">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
+                            className="h-4 md:h-5 w-4 md:w-5"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor">
@@ -38,24 +38,24 @@ const NavBar = () => {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        className="ml-5 menu-sm dropdown-content bg-base-100 rounded-md z-[1] mt-3 w-32 p-2 shadow">
                         {links}
                     </ul>
                 </div>
-                <a className="text-2xl font-black">Book Vibe</a>
+                <a className="text-3xl font-black">Book Vibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu-horizontal gap-4">
                     {links}
                 </ul>
             </div>
-            <div className="navbar-end gap-3">
+            <div className="navbar-end gap-1 md:gap-3">
                 <label className="swap swap-rotate">
                     {/* this hidden checkbox controls the state */}
                     <input onChange={handleToggleTheme} type="checkbox" className="theme-controller" checked={theme === 'synthwave'} />
                     {/* sun icon */}
                     <svg
-                        className="swap-off h-8 w-8 fill-current"
+                        className="swap-off h-4 md:h-8 w-4 md:w-8 fill-current"
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24">
                         <path
@@ -63,16 +63,16 @@ const NavBar = () => {
                     </svg>
                     {/* moon icon */}
                     <svg
-                        className="swap-on h-8 w-8 fill-current"
+                        className="swap-on h-4 md:h-8 w-4 md:w-8 fill-current"
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24">
                         <path
                             d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
                     </svg>
                 </label>
-                <div className="flex gap-3">
-                    <a className="px-6 text-white py-[9px] rounded-md bg-[#23BE0A] hover:bg-green-500 cursor-pointer duration-100">Sign In</a>
-                    <a className="px-6 text-white py-[9px] rounded-md bg-[#59C6D2] hover:bg-[#48d7e7] cursor-pointer duration-100">Sign Up</a>
+                <div className="flex gap-1 md:gap-3">
+                    <a className="text-xs md:text-base md:px-6 px-2 text-white py-1 md:py-[9px] rounded-sm md:rounded-md bg-[#23BE0A] hover:bg-green-500 cursor-pointer duration-100">Sign In</a>
+                    <a className="text-xs md:text-base md:px-6 px-2 text-white py-1 md:py-[9px] rounded-sm md:rounded-md bg-[#59C6D2] hover:bg-[#48d7e7] cursor-pointer duration-100">Sign Up</a>
                 </div>
             </div>
         </div>
